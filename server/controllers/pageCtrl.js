b7b57c60f8714c121e4401fc583d4361b4538db7
Kellon Parkinson <kellon.digital@gmail.com>
2022-11-02 14:56:18 -0600

    added js and css to controller

--- server/controllers/pageCtrl.js
+++ server/controllers/pageCtrl.js
@@ -3,5 +3,11 @@ const path = require('path')
 module.exports = {
     home: (req, res) => {
         res.sendFile(path.join(__dirname, '../../index.html'))
+    },
+    javaScript: (req, res) => {
+        res.sendFile(path.join(__dirname, '../../index.js'))
+    },
+    css: (req, res) => {
+        res.sendFile(path.join(__dirname, '../../index.css'))
     }
 }
--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const { home } = require('./controllers/pageCtrl')
+const { home, javaScript, css } = require('./controllers/pageCtrl')
 
 app.get('/', home)
+app.get('/js', javaScript)
+app.get('/css', css)
 
 const { PORT } = process.env
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
