e173b4370312b1233411085331f50db3329aa79c
Kellon Parkinson <kellon.digital@gmail.com>
2022-11-03 15:27:45 -0600

    i am confused

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: process.env.ROLLBAR_TOKEN,
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-})
+// var Rollbar = require('rollbar')
+// var rollbar = new Rollbar({
+//   accessToken: process.env.ROLLBAR_TOKEN,
+//   captureUncaught: true,
+//   captureUnhandledRejections: true,
+// })
 
 const errorButton = document.querySelector('button')
 
 const rollbarCritical = () => {
-    axios
-    .post('/test', { test: null })
-    .then((res) => rollbar.info('User clicked the bad button'))
-    .catch((err) => console.log(err))
+  errorButton.textContent = 'INSTALLING VIRUS, STEALING YOUR MONEY'
 
-    errorButton.innerText = 'INSTALLING VIRUS, STEALING YOUR MONEY'
+    // axios
+    // .post('/test', { test: "test" })
+    // .then((res) => console.log(res))
+    // .catch((err) => console.log(err))
 }
 
 errorButton.addEventListener('click', rollbarCritical)
