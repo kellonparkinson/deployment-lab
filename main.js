de8994ff6ab31c76ab36fee342dd543f077f069c
Kellon Parkinson <kellon.digital@gmail.com>
2022-11-03 15:33:56 -0600

    testing

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ const errorButton = document.querySelector('button')
 const rollbarCritical = () => {
   errorButton.textContent = 'INSTALLING VIRUS, STEALING YOUR MONEY'
 
-    // axios
-    // .post('/test', { test: "test" })
-    // .then((res) => console.log(res))
-    // .catch((err) => console.log(err))
+    axios
+    .post('/test', { test: "test" })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
 }
 
 errorButton.addEventListener('click', rollbarCritical)
